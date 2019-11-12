17a24d1c3c15454667a4637c8a1f6f10791d1d9e
igorbragaia <igor.bragaia@gmail.com>
2019-11-12 14:36:38 -0300

    adding mockup to server / route

--- server/app.js
+++ server/app.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  let objeto = {
-    a: 2
+app.get('/', (req, res) => {
+  let response = {
+    values: [
+      { x:1, y: 2 },
+      { x:2, y: 10 },
+      { x:3, y: 5 },
+      { x:4, y: 6 },
+      { x:5, y: 7 },
+      { x:6, y: 11 },
+      { x:7, y: 9 },
+      { x:8, y: 1 },
+    ]
   };
-  res.send(objeto);
+  res.status(200).send(response);
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
