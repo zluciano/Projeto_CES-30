bbd01b378d6d3fad3e3949f91c4fb4d39645d3ec
igor-ribeiiro <ribeiro_igor@hotmail.com>
2019-11-25 19:22:25 -0300

    Bragaia faz o resto

--- server/app.js
+++ server/app.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const oracledb = require('oracledb');
+
+oracledb.getConnection({
+    user          : "hr",
+    password      : "welcome",
+    connectString : "localhost/XE"
+  },
+
+  function(err, connection) {
+    if (err) { console.error(err); return; }
+    connection.execute(
+      "SELECT department_id, department_name "
+    + "FROM departments "
+    + "WHERE department_id < 70 "
+    + "ORDER BY department_id",
+      function(err, result) {
+        if (err) { console.error(err); return; }
+        console.log(result.rows);
+      });
+  });
+
 
 let app = express()
   .use(cors())
