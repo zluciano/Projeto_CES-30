990b556f48581dd3ec1fe78eb677b5f23217f205
Isabelle Ferreira <isabelle.ferreira3000@gmail.com>
2019-11-25 22:39:28 -0300

    correcting insert data

--- database/insert_data.py
+++ database/insert_data.py
@@ -1,32 +1,40 @@
-import pandas as pd
-
 table_name = "escola"
-# table_filepath = "tables/" + str(table_name) + ".csv"
-table_filepath = 'tables/escola_exemplo.csv'
+table_filepath = "tables/" + str(table_name) + ".csv"
+# table_filepath = 'tables/escola_exemplo.csv'
 
-dataset = pd.read_csv(table_filepath, delimiter=";", engine='python')
-dataset = dataset.dropna(axis=0, how='any')
-
-print("DATASET", table_name.upper(), "READ FINISHED\n")
-print(dataset.head())
-
-columns = []
-for col in dataset.columns:
-    columns.append(col)
-
-print("\nCOLUMNS", columns)
-print("")
-
-insert_queries_filepath = 'queries/insert_escola.sql'
+insert_queries_filepath = "SQLs/insert/insert_" + table_name + ".sql"
 
 f = open(insert_queries_filepath, "w+")
 
-for index, row in dataset.iterrows():
-    query = "INSERT INTO " + str(table_name.upper()) + " VALUES ("
-    for col in columns:
-        query += str(row[col])
-        if col != columns[-1]:
-            query += ", "
-    query += ");"
+count = 1
+with open(table_filepath, 'r', encoding='utf-8-sig') as table_file:
+    row = table_file.readline()
+    columns = row.split(";")
+    columns[-1] = columns[-1].split("\n")[0]
 
-    f.write(query + "\n")
+    row = table_file.readline()
+    while row:
+        count += 1
+        if count % 1000 == 0:
+            print("line", count)
+
+        query = "INSERT INTO " + str(table_name.upper()) + " VALUES ("
+        row_split = row.split(";")
+        row_split[-1] = row_split[-1].split("\n")[0]
+
+        for value in row_split:
+
+            if value == row_split[1]:
+                query += "'" + str(value) + "'"
+            else:
+                query += str(value)
+
+            if value != row_split[-1]:
+                query += ", "
+        query += ");\n"
+
+        f.write(query)
+
+        row = table_file.readline()
+
+f.close()
