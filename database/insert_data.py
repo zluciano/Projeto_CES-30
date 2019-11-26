c2b23d3fdd1ced413bdebbb4e7b9a06c92f0d9a3
Isabelle Ferreira <isabelle.ferreira3000@gmail.com>
2019-11-25 23:29:22 -0300

    insert do enem_escola faltando linhas

--- database/insert_data.py
+++ database/insert_data.py
@@ -1,15 +1,15 @@
 # table_name = "escola"
-# table_name = "enem_escola"
-table_name = "municipio"
-# table_filepath = "tables/" + str(table_name) + ".csv"
-table_filepath = "tables/" + str(table_name) + "_exemplo" + ".csv"
+table_name = "enem_escola"
+# table_name = "municipio"
+table_filepath = "tables/" + str(table_name) + ".csv"
+# table_filepath = "tables/" + str(table_name) + "_exemplo" + ".csv"
 
 insert_queries_filepath = "SQLs/insert/insert_" + table_name + ".sql"
 
 f = open(insert_queries_filepath, "w+")
 
 count = 1
-with open(table_filepath, 'r', encoding='utf-8-sig') as table_file:
+with open(table_filepath, 'r', encoding='utf-8-sig', errors='ignore') as table_file:
     row = table_file.readline()
     columns = row.split(";")
     columns[-1] = columns[-1].split("\n")[0]
@@ -27,12 +27,20 @@ with open(table_filepath, 'r', encoding='utf-8-sig') as table_file:
         for value in row_split:
             if table_name == "escola" or table_name == "enem_escola":
                 if value == row_split[1]:
-                    query += "q'" + str(value) + "'"
+                    index = value.find('\'')
+                    if index != -1:
+                        value = value[:index] + '\'' + value[index:]
+                    index = -1
+                    query += "'" + str(value) + "'"
                 else:
                     query += str(value)
             elif table_name == "municipio":
                 if value == row_split[1] or value == row_split[2]:
-                    query += "q'" + str(value) + "'"
+                    index = value.find('\'')
+                    if index != -1:
+                        value = value[:index] + '\'' + value[index:]
+                    index = -1
+                    query += "'" + str(value) + "'"
                 else:
                     query += str(value)
 
